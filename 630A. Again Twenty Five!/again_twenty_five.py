1ac72a910d130fcc9acee4cfbb7ce5a7776fee93
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-10-20 18:03:43 +0530

    Create again_twenty_five.py

---
+++ 630A. Again Twenty Five!/again_twenty_five.py
@@ -0,0 +1,52 @@
+# By aniketmondal1210, contest: Experimental Educational Round: VolBIT Formulas Blitz, problem: (A) Again Twenty Five!, Accepted, #, Copy
+t = int(input())
+print(5**2)
+
+'''
+→Judgement Protocol
+Test: #1, time: 62 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+2
+Output
+25
+Answer
+25
+Checker Log
+ok Accepted
+Test: #2, time: 61 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+7
+Output
+25
+Answer
+25
+Checker Log
+ok Accepted
+Test: #3, time: 62 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+1000000000000000000
+Output
+25
+Answer
+25
+Checker Log
+ok Accepted
+Test: #4, time: 77 ms., memory: 0 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+2000000000000000000
+Output
+25
+Answer
+25
+Checker Log
+ok Accepted
+Test: #5, time: 46 ms., memory: 8 KB, exit code: 0, checker exit code: 0, verdict: OK
+Input
+987654321012345678
+Output
+25
+Answer
+25
+Checker Log
+ok Accepted
+'''
